a3acac3822c0e40f6bb9cf3190c57e58e6cebf9f
Soumya <soumyayad5200@gmail.com>
2026-03-21 13:30:10 +0530

    fix deploy issue

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const connectDB = require("./database/db");
@@ -18,7 +17,7 @@ const safeSpotRoutes = require("./routes/safeSpotRoutes");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
@@ -26,7 +25,7 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
